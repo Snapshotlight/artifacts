06a543335f1cdb600a53da5cb14146a5766fb507
Wouter Pauwels <37985539+ochotonida@users.noreply.github.com>
2024-05-30 10:19:05 +0200

    Fix item config search

--- common/src/main/java/artifacts/config/screen/ItemSubCategoryListEntry.java
+++ common/src/main/java/artifacts/config/screen/ItemSubCategoryListEntry.java
@@ -1,7 +1,5 @@
 package artifacts.config.screen;
 
-import com.google.common.collect.Iterators;
-import me.shedaniel.clothconfig2.api.AbstractConfigEntry;
 import me.shedaniel.clothconfig2.api.AbstractConfigListEntry;
 import me.shedaniel.clothconfig2.gui.entries.SubCategoryListEntry;
 import net.minecraft.ChatFormatting;
@@ -13,8 +11,6 @@ import net.minecraft.network.chat.MutableComponent;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
 
-import java.util.AbstractList;
-import java.util.Iterator;
 import java.util.List;
 
 public class ItemSubCategoryListEntry extends SubCategoryListEntry {
@@ -25,35 +21,9 @@ public class ItemSubCategoryListEntry extends SubCategoryListEntry {
         // noinspection deprecation
         super(item.getDescription(), List.copyOf(entries), false);
         this.stack = new ItemStack(item);
-    }
-
-    @Override
-    public Iterator<String> getSearchTags() {
+        List<String> searchTags = List.of(getFieldName().getString().split(" "));
         // noinspection unchecked
-        return Iterators.filter(super.getSearchTags(), tag -> getValue().stream()
-                .map(AbstractConfigEntry::getSearchTags)
-                .noneMatch(iterator -> Iterators.any(iterator, s -> tag != null && tag.equals(s))));
-    }
-
-    @SuppressWarnings("rawtypes")
-    @Override
-    public List<AbstractConfigListEntry> filteredEntries() {
-        return new AbstractList<>() {
-            public Iterator<AbstractConfigListEntry> iterator() {
-                return Iterators.filter(
-                        ItemSubCategoryListEntry.this.getValue().iterator(),
-                        (entry) -> entry.isDisplayed() && ItemSubCategoryListEntry.this.getConfigScreen() != null
-                );
-            }
-
-            public AbstractConfigListEntry get(int index) {
-                return Iterators.get(this.iterator(), index);
-            }
-
-            public int size() {
-                return Iterators.size(this.iterator());
-            }
-        };
+        getValue().forEach(value -> value.appendSearchTags(searchTags));
     }
 
     @Override
