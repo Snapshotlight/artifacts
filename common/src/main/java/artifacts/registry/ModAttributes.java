4205bb175a8903b6519a529f55cb688086596770
Wouter Pauwels <37985539+ochotonida@users.noreply.github.com>
2024-06-07 12:12:51 +0200

    Update attribute ranges

--- common/src/main/java/artifacts/registry/ModAttributes.java
+++ common/src/main/java/artifacts/registry/ModAttributes.java
@@ -21,8 +21,8 @@ public class ModAttributes {
     public static final Holder<Attribute> ENTITY_EXPERIENCE = addPlayerAttribute("entity_experience", 1, 0, 64);
     public static final Holder<Attribute> VILLAGER_REPUTATION = addPlayerAttribute("villager_reputation", 0, 0, 1024);
 
-    public static final Holder<Attribute> ATTACK_BURNING_DURATION = addGenericAttribute("attack_burning_duration", 0, 0, 64);
-    public static final Holder<Attribute> ATTACK_DAMAGE_ABSORPTION = addGenericAttribute("attack_damage_absorption", 0, 0, 64);
+    public static final Holder<Attribute> ATTACK_BURNING_DURATION = addGenericAttribute("attack_burning_duration", 0, 0, 60);
+    public static final Holder<Attribute> ATTACK_DAMAGE_ABSORPTION = addGenericAttribute("attack_damage_absorption", 0, 0, 1024);
     public static final Holder<Attribute> DRINKING_SPEED = addGenericAttribute("drinking_speed", 1, 1, Double.MAX_VALUE);
     public static final Holder<Attribute> EATING_SPEED = addGenericAttribute("eating_speed", 1, 1, Double.MAX_VALUE);
     public static final Holder<Attribute> FLATULENCE = addGenericAttribute("flatulence", 0, 0, 1);
@@ -32,7 +32,7 @@ public class ModAttributes {
     public static final Holder<Attribute> MOVEMENT_SPEED_ON_SNOW = addGenericAttribute("movement_speed_on_snow", 1, 0, 1024);
     public static final Holder<Attribute> SLIP_RESISTANCE = addGenericAttribute("slip_resistance", 0, 0, 1);
     public static final Holder<Attribute> SPRINTING_SPEED = addGenericAttribute("sprinting_speed", 1, 1, 1024);
-    public static final Holder<Attribute> SPRINTING_STEP_HEIGHT = addGenericAttribute("sprinting_step_height", 0, 0, 8);
+    public static final Holder<Attribute> SPRINTING_STEP_HEIGHT = addGenericAttribute("sprinting_step_height", 0, 0, 10);
     public static final Holder<Attribute> SWIM_SPEED = PlatformServices.platformHelper.getSwimSpeedAttribute();
 
     public static Holder<Attribute> addPlayerAttribute(String name, double d, double min, double max) {
